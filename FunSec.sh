d876fb14e13b1ca4b3d88bf5eb0dac945b6b8b96
Lonewolfenrir <baptista@lonewolf-pc.alunos.fc.ul.pt>
2017-01-04 14:00:18 +0000

    First Release version 1.0

---
+++ FunSec.sh
@@ -0,0 +1,322 @@
+#!/usr/bin/env bash
+
+
+#	FunSec - Fungi Secreted Proteins Predictor Pipeline.
+#	Copyright (C) 2016 João Baptista <baptista.joao33@gmail.com>
+#
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# To do list:
+#
+# - Check the bin directory for the correct programs files.
+# - Report sequences or no?
+
+# Version
+
+version() {
+echo -e "Version: 1.0
+Last Updated: 03-01-16"
+}
+
+# Help text
+
+help_text() {
+echo -e "Usage:
+
+        ./FunSec.sh -[OPTION] [ARGUMENT]
+
+Options:
+
+        -i,		Input directory.
+        -o,		Output directory.
+        -h,		Displays this message.
+        -w,		Threshold number for the program WolfPsort 0.2. Must be in the range of 1-30, the default value is 17.
+        -v,		Displays version.
+
+        Both options -i and -o must be specified. For more information read the README.md file."
+}
+
+# SignalP 4.1
+
+signalp() {
+echo -e "\nRunning SignalP 4.1.\n"
+mkdir "$OUTPUT"/FunSec_Output/SignalP
+ls "$INPUT"/ | while read i
+do
+  ./bin/signalp-4.1/signalp -m "$OUTPUT"/FunSec_Output/SignalP/"$i" "$INPUT"/"$i" 2> /dev/null | \
+  awk '{if ($1 != "#") print $1}'
+done
+echo -e "\nFinished. (Runtime - $SECONDS seconds)"
+}
+
+# TMHMM 2.0c after SignalP 4.1
+
+tmhmm() {
+echo -e "\nRunning TMHMM 2.0 with SignalP 4.1 mature sequences.\n"
+mkdir "$OUTPUT"/FunSec_Output/TMHMM
+ls "$OUTPUT"/FunSec_Output/SignalP/ | while read i
+do
+  ./bin/tmhmm-2.0c/bin/tmhmm -short "$OUTPUT"/FunSec_Output/SignalP/"$i" | \
+  awk '{if ($5=="PredHel=0") print $1}' | \
+  sort | \
+  tee "$OUTPUT"/FunSec_Output/TMHMM/"$i"
+done
+ls ./ | grep "TMHMM"| while read i  # Remove directories created by TMHMM
+do
+  rm -rf "$i"
+done
+echo -e "\nFinished. (Runtime - $SECONDS seconds)"
+}
+
+# Phobius 1.01
+
+phobius() {
+echo -e "\nRunning Phobius 1.01.\n"
+mkdir "$OUTPUT"/FunSec_Output/Phobius
+ls "$INPUT"/ | while read i
+do
+	./bin/phobius/phobius.pl -short < "$INPUT"/"$i" 2> /dev/null | \
+	awk '{if ($2 == "0" && $3 =="Y") print $1}' | \
+	sort | \
+  tee "$OUTPUT"/FunSec_Output/Phobius/"$i"
+done
+echo -e "\nFinished. (Runtime - $SECONDS seconds)"
+}
+
+# SignalP 4.1 + TMHMM 2.0c and Phobius 1.01
+
+signalp_tmhmm_phobius() {
+echo -e "\nSelecting the common sequences found by SignalP 4.1 + TMHMM 2.0 and Phobius 1.01."
+mkdir "$OUTPUT"/FunSec_Output/SignalP_TMHMM_Phobius "$OUTPUT"/FunSec_Output/SignalP_TMHMM_Phobius/Headers
+ls "$OUTPUT"/FunSec_Output/TMHMM/ | while read i  # The path is "$OUTPUT"/FunSec_Output/TMHMM/"$i" because the output of phobius may contain empty files.
+do
+  comm -12 "$OUTPUT"/FunSec_Output/Phobius/"$i" "$OUTPUT"/FunSec_Output/TMHMM/"$i" > "$OUTPUT"/FunSec_Output/SignalP_TMHMM_Phobius/Headers/"$i"
+done
+echo -e "\nFinished. (Runtime - $SECONDS seconds)"
+}
+
+# SignalP + TMHMM and Phobius Sequences Retriever
+
+signalp_tmhmm_phobius_retriever() {
+echo -e "\nRetriving the common sequences of SignalP 4.1 + TMHMM 2.0 and Phobius 1.01."
+mkdir "$OUTPUT"/FunSec_Output/SignalP_TMHMM_Phobius/Sequences
+ls "$OUTPUT"/FunSec_Output/SignalP_TMHMM_Phobius/Headers | while read i
+do
+	for f in $(cat "$OUTPUT"/FunSec_Output/SignalP_TMHMM_Phobius/Headers/"$i")
+	do
+		awk -v f="$f" 'BEGIN {RS=">"} {if ($1 == f) print RS$0}' "$INPUT"/"$i" | \
+    sed '/^$/d' >> "$OUTPUT"/FunSec_Output/SignalP_TMHMM_Phobius/Sequences/"$i"
+	done
+done
+echo -e "\nFinished. (Runtime - $SECONDS seconds)"
+}
+
+# WolfPsort 0.2
+
+wolfpsort() {
+echo -e "\nRunning WolfPsort 0.2.\n"
+mkdir "$OUTPUT"/FunSec_Output/WolfPsort
+ls "$OUTPUT"/FunSec_Output/SignalP_TMHMM_Phobius/Sequences/ | while read i
+do
+	./bin/WoLFPSort-master/bin/runWolfPsortSummary fungi < "$OUTPUT"/FunSec_Output/SignalP_TMHMM_Phobius/Sequences/"$i" | \
+	sed 's/,//g' | \
+	awk -v w="$THRESHOLD_WOLFPSORT" 'BEGIN {FS=" "} {if ($2 == "extr" && $3 > w) print $1}' | \
+	sort | \
+  tee "$OUTPUT"/FunSec_Output/WolfPsort/"$i"
+done
+echo -e "\nFinished. (Runtime - $SECONDS seconds)"
+}
+
+# ProtComp 9.0
+
+protcomp() {
+echo -e "\nRunning ProtComp 9.0.\n"
+mkdir "$OUTPUT"/FunSec_Output/ProtComp
+ls "$OUTPUT"/FunSec_Output/SignalP_TMHMM_Phobius/Sequences/ | while read i
+do
+	./bin/lin/pc_fm "$OUTPUT"/FunSec_Output/SignalP_TMHMM_Phobius/Sequences/"$i" -NODB -NOOL | \
+	awk 'BEGIN {RS="Seq name: "} /Integral Prediction of protein location: Membrane bound Extracellular/ || /Integral Prediction of protein location: Extracellular/ {print $1}' | \
+	sort | \
+  tee "$OUTPUT"/FunSec_Output/ProtComp/"$i"
+done
+echo -e "\nFinished. (Runtime - $SECONDS seconds)"
+}
+
+# WolfPsort and ProtComp
+
+wolfpsort_protcomp() {
+echo -e "\nSelecting the common sequences found by WolfPsort 0.2 and ProtComp 9.0."
+mkdir "$OUTPUT"/FunSec_Output/WolfPsort_ProtComp "$OUTPUT"/FunSec_Output/WolfPsort_ProtComp/Headers
+ls "$OUTPUT"/FunSec_Output/SignalP_TMHMM_Phobius/Sequences/ | while read i
+do
+	comm -12 "$OUTPUT"/FunSec_Output/WolfPsort/"$i" "$OUTPUT"/FunSec_Output/ProtComp/"$i" > "$OUTPUT"/FunSec_Output/WolfPsort_ProtComp/Headers/"$i"
+done
+echo -e "\nFinished. (Runtime - $SECONDS seconds)"
+}
+
+# WolfPsort and ProtComp Sequences Retriever
+
+wolfpsort_protcomp_retriever() {
+echo -e "\nRetriving the common sequences of WolfPsort 0.2 and ProtComp 9.0."
+mkdir "$OUTPUT"/FunSec_Output/WolfPsort_ProtComp/Sequences
+ls "$OUTPUT"/FunSec_Output/WolfPsort_ProtComp/Headers/ | while read i
+do
+	for f in $(cat "$OUTPUT"/FunSec_Output/WolfPsort_ProtComp/Headers/"$i")
+	do
+		awk -v f="$f" 'BEGIN {RS=">"} {if ($1 == f) print RS$0}' "$OUTPUT"/FunSec_Output/SignalP_TMHMM_Phobius/Sequences/"$i" | \
+		sed '/^$/d' >> "$OUTPUT"/FunSec_Output/WolfPsort_ProtComp/Sequences/"$i"
+	done
+done
+echo -e "\nFinished. (Runtime - $SECONDS seconds)"
+}
+
+# Ps-scan 1.86
+
+ps_scan() {
+echo -e "\nRunning Ps-scan 1.86.\n"
+mkdir "$OUTPUT"/FunSec_Output/Ps-scan
+ls "$OUTPUT"/FunSec_Output/WolfPsort_ProtComp/Sequences/ | while read i
+do
+	./bin/ps_scan/ps_scan.pl -p "[KRHQSA]-[DENQ]-E-L>" "$OUTPUT"/FunSec_Output/WolfPsort_ProtComp/Sequences/"$i" | \
+	awk 'BEGIN{RS=">"} {print $1}' | \
+	sed '/^$/d' | \
+	sort | \
+  tee "$OUTPUT"/FunSec_Output/Ps-scan/"$i"
+done
+echo -e "\nFinished. (Runtime - $SECONDS seconds)"
+}
+
+# Ps-scan Sequences Remover
+
+ps_scan_remover() {
+echo -e "\nRemoving the sequences found by Ps-scan 1.86 from the WolfPsort 0.2 and ProtComp 9.0 common sequences."
+mkdir "$OUTPUT"/FunSec_Output/Final "$OUTPUT"/FunSec_Output/Final/Headers
+ls "$OUTPUT"/FunSec_Output/Ps-scan/ | while read i
+do
+	comm -13 "$OUTPUT"/FunSec_Output/Ps-scan/"$i" "$OUTPUT"/FunSec_Output/WolfPsort_ProtComp/Headers/"$i" > "$OUTPUT"/FunSec_Output/Final/Headers/"$i"
+done
+echo -e "\nFinished. (Runtime - $SECONDS seconds)"
+}
+
+# Ps-scan Sequences Retriever
+
+ps_scan_retriever() {
+echo -e "\nRetriving final secreted sequences."
+mkdir "$OUTPUT"/FunSec_Output/Final/Sequences
+ls "$OUTPUT"/FunSec_Output/Final/Headers/ | while read i
+do
+	for f in $(cat "$OUTPUT"/FunSec_Output/Final/Headers/"$i")
+	do
+		awk -v f="$f" 'BEGIN{RS=">"} {if ($1 == f) print RS$0}' "$OUTPUT"/FunSec_Output/WolfPsort_ProtComp/Sequences/"$i" | \
+		sed '/^$/d' >> "$OUTPUT"/FunSec_Output/Final/Sequences/"$i"
+	done
+done
+echo -e "\nFinished. (Runtime - $SECONDS seconds)"
+}
+
+# Variables
+
+INPUT=""  # Input directory
+OUTPUT="" # Output directory
+FLAG_INPUT=0  # If Input is true or false
+FLAG_OUTPUT=0	# If Output is true or false
+THRESHOLD_WOLFPSORT=17	# Threshold for WolfPsort program
+
+# Command options and arguments parser
+
+while getopts :i:o:w:vh OPT
+do
+	case $OPT in
+		i)
+			INPUT=$OPTARG
+			if [ -d "$INPUT" ]	# if input is a directory
+			then
+				if [ "$(ls -A "$INPUT")" ]	# if the directory is empty or not
+				then
+					FLAG_INPUT=1
+				else
+					echo -e "\n$INPUT is empty."
+					exit 1
+				fi
+			else
+				echo -e "\n$INPUT doesn't exist or it's not a directory."
+				exit 1
+			fi
+			;;
+		o)
+			OUTPUT=$OPTARG
+			if [ -d "$OUTPUT" ]	# if input is a directory
+			then
+				FLAG_OUTPUT=1
+				if [ -d "$OUTPUT"/FunSec_Output ]	# if $OUTPUT/FunSec_Output exists it will ask for permission to overwrite.
+				then
+					echo -e "\n$OUTPUT/FunSec_Output already exists, this will overwrite it. Do you want to continue? [Y/n]"
+					read FLAG_OVERWRITE
+					if [ "$FLAG_OVERWRITE" == "yes" ] || [ "$FLAG_OVERWRITE" == "y" ] || [ "$FLAG_OVERWRITE" == "Yes" ] || [ "$FLAG_OVERWRITE" == "Y" ]
+					then
+						echo -e "\nOverwriting $OUTPUT/FunSec_Output."
+						rm -rf "$OUTPUT"/FunSec_Output && mkdir "$OUTPUT"/FunSec_Output
+					else
+						echo -e "\nExisting..."
+						exit 0
+					fi
+				else
+					echo -e "\nCreating $OUTPUT/FunSec_Output."
+					mkdir "$OUTPUT"/FunSec_Output
+				fi
+			else
+				echo -e "\n$OUTPUT doesn't exist or it's not a directory."
+				exit 1
+			fi
+			;;
+		w)
+			THRESHOLD_WOLFPSORT=$OPTARG
+			if [[ "$THRESHOLD_WOLFPSORT" -gt 0 && "$THRESHOLD_WOLFPSORT" -lt 31 ]]
+			then
+				echo -e "\nThe threshold number for WolfPsort is set to $THRESHOLD_WOLFPSORT."
+			else
+				echo -e "\nThe threshold number for WolfPsort must be in the range of 1-30. Use -h for more information."
+				exit 1
+			fi
+			;;
+		h)
+			help_text
+			exit 0
+			;;
+		v)
+			version
+			exit 0
+			;;
+		\?)
+			echo -e "\nInvalid option: -$OPTARG. Use -h for more information."
+			exit 1
+			;;
+		:)
+			echo -e "\nThe option -$OPTARG needs an argument. Use -h for more information."
+			exit 1
+			;;
+	esac
+done
+
+if [ $FLAG_INPUT == 1 ] && [ $FLAG_OUTPUT == 1 ]
+then
+  cd $(dirname $0)
+	echo -e "\nThe program $0 is running, it may take a while. I encourage you to go outside or to pet some animal."
+  signalp && tmhmm && phobius && signalp_tmhmm_phobius && signalp_tmhmm_phobius_retriever && wolfpsort && protcomp && wolfpsort_protcomp && wolfpsort_protcomp_retriever && ps_scan && ps_scan_remover && ps_scan_retriever
+  exit 0
+else
+	echo -e "\nBoth options -i and -o must be specified. Use -h for more information."
+	exit 1
+fi
---
+++ README.md
@@ -0,0 +1,168 @@
+# FunSec - Fungi Secreted Proteins Predictor Pipeline #
+
+## Description ##
+
+This script was designed to automate the prediction of fungi secreted proteins from a single or a batch of FASTA files using a complex pipeline that uses programs for the prediction of signal peptides, trans-membrane regions, subcellular localization and endoplasmic reticulum targeting motif. It can also be used for the prediction of complete fungi secretomes when given their proteomes.
+
+To predict signal peptides and trans-membrane regions we used two different methods and only the proteins that were predicted by both methods were selected for further analysis. The first method uses SignalP 4.1 and TMHMM 2.0 to predict signal peptides and trans-membrane regions respectively. The second method uses Phobius 1.01 which predicts both signal peptides and trans-membrane regions. By using two methods that rely in different approaches for their predictions we were able to reduce the rate of false positives. The subcellular localization prediction was made using two predictors, WolfPsort 0.2 and ProtComp 9.0 and their output was combined. Finally, Ps-scan 1.86 with the profile of the Prosite motif entry, PS00014, was used to predict the endoplasmic reticulum proteins.
+
+## Requirements ##
+
+First, this script needs the programs described above, so users will have to install each program individually into the bin directory of this script, with the following structure:
+
+```
+-bin
+  |-lin
+  |-phobius
+  |-ps_scan
+  |-signalp-4.1
+  |-tmhmm-2.0c
+  |-WoLFPSort-master
+```
+
+This script only runs in **Linux** and Perl needs to be installed.
+
+#### SignalP 4.1 ####
+
+First to download SignalP 4.1, visit <http://www.cbs.dtu.dk/cgi-bin/nph-sw_request?signalp> and download the program for the Unix platform.
+
+You should receive a tar.gz file. To untar the file use the following command line:
+
+```
+tar -zxvf FILE.tar.gz
+```
+
+This will create a directory called signalp-4.1. You must move this directory into the bin directory of this script.
+
+Now you must edit the file signalp in the directory bin/signalp-4.1/. You should see something like this:
+
+```
+# full path to the signalp-4.1 directory on your system (mandatory)
+BEGIN {
+    $ENV{SIGNALP} = '/usr/cbs/bio/src/signalp-4.1';
+}
+
+# determine where to store temporary files (must be writable to all users)
+my $outputDir = "/var/tmp";
+
+# max number of sequences per run (any number can be handled)
+my $MAX_ALLOWED_ENTRIES=10000;
+```
+
+The path to the signalp-4.1 directory must be changed, if you don't know the path to the directory you can use the command 'pwd' in your signalp-4.1 directory and paste it on the configuration. If you are dealing with more than 10000 sequences per file, you must edit the value for the maximum allowed entries.
+
+For more instructions go to <http://www.cbs.dtu.dk/services/doc/signalp-4.1.readme>.
+
+#### TMHMM 2.0 ####
+
+To download TMHMM 2.0 for the Unix platform visit <http://www.cbs.dtu.dk/cgi-bin/sw_request?tmhmm>.
+
+You should receive a tar.gz file. To untar the file use the following command line:
+
+```
+tar -zxvf FILE.tar.gz
+```
+
+This will create a directory called tmhmm-2.0c. You must move this directory into the bin directory of this script.
+
+Now you will probably have to edit the first line of the files tmhmm and tmhmmformat.pl in the directory bin/tmhmm-2.0c/bin/. Run the following command:
+
+```
+which perl
+```
+
+If the output isn't /usr/local/bin/perl, then you must change the path in the first lines of the files with the one given by the command above.
+
+#### Phobius 1.01 ####
+
+To download Phobius 1.01 visit <http://software.sbc.su.se/cgi-bin/request.cgi?project=phobius>.
+
+You should receive a tar.gz file. To untar the file use the following command line:
+
+```
+tar -zxvf FILE.tar.gz
+```
+
+This will create a directory called phobius. You must move this directory into the bin directory of this script.
+
+#### WolfPsort 0.2 ####
+
+To download WolfPsort you can go to <https://github.com/fmaguire/WoLFPSort> or use the following command:
+
+```
+wget https://github.com/fmaguire/WoLFPSort/archive/master.zip
+```
+
+You should receive a zip file. To unzip the file use the following command line:
+
+```
+unzip FILE.zip
+```
+
+Then move the file WoLFPSort-master to the bin directory of this script.
+
+#### ProtComp 9.0 ####
+
+To download ProtComp you can go to <http://linux5.softberry.com/cgi-bin/download.pl?file=protcompan>.
+
+You should receive a tar.bz2 file. To untar the file use the following command line:
+
+```
+tar -xvjf FILE.tar.bz2
+```
+
+This will create a directory called lin. You must move this directory into the bin directory of this script.
+
+#### Ps-scan 1.86 ####
+
+To download Ps-scan 1.86 you can go to <ftp://ftp.expasy.org/databases/prosite/ps_scan/ps_scan_linux_x86_elf.tar.gz> or use the following command:
+
+```
+wget ftp://ftp.expasy.org/databases/prosite/ps_scan/ps_scan_linux_x86_elf.tar.gz
+```
+
+You should receive a tar.gz file. To untar the file use the following command line:
+
+```
+tar -zxvf FILE.tar.gz
+```
+
+This will create a directory called ps_scan. You must move this directory into the bin directory of this script.
+
+## Usage ##
+
+```
+./SecPipe.sh -[OPTION] [ARGUMENT]
+```
+
+This script only works with FASTA format files. The headers of the sequences also should similar to Uniprot ID for example: sp|A7F996|3DHQ_SCLS1.
+
+## Options ##
+
+```
+-i,		Input directory.
+-o,		Output directory.
+-h,		Displays help message.
+-w,		Threshold number for the program WolfPsort 0.2. Must be in the range of 1-30, the default value is 17.
+-v,		Displays version.
+
+Both options -i and -o must be specified.
+```
+
+## Citation ##
+
+Coming.
+
+## Contact ##
+
+For further information or feedback please contact baptista.joao33@gmail.com.
+
+## License ##
+
+GPLv3, see LICENSE file for more information.
+
+## Version ##
+
+Version: 1.0
+
+Last Updated: 03/01/16
