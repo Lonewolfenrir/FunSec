9d4bba17b231bd730f079b7572db1454efcbf2e0
João Baptista <Lonewolfenrir@users.noreply.github.com>
2021-03-06 17:59:23 +0000

    Update Fasta_Parser.py

--- bin/FunSec/Fasta_Parser.py
+++ bin/FunSec/Fasta_Parser.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 
-# FunSec - Fungal Secreted Proteins (or Secretome) Predictor Pipeline.
-# Copyright (C) 2016 João Baptista <baptista.joao33@gmail.com>
+# FunSec - Fungal Secreted Proteins (or Secretome) Prediction Pipeline.
+# Copyright (C) 2019 João Baptista <baptista.joao33@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,4 +18,34 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
+from sys import argv
 
+
+def main():
+    try:
+        with open(argv[1], "r") as f:
+            if str(f.read(1)) == ">":
+                f.seek(0)
+            else:
+                print("ERROR")
+                exit(1)
+            for line in f:
+                line = line.rstrip()
+                if line.startswith(">"):
+                    if len(line) > 21:
+                        print("ERROR1")
+                        exit(1)
+                    elif " " in line:
+                        print("ERROR2")
+                        exit(1)
+                else:
+                    for c in line:
+                        if c.upper() not in "ACDEFGHIKLMNPQRSTVWYX":
+                            print("ERROR3")
+                            exit(1)
+    except UnicodeDecodeError:
+        print("ERROR")
+
+
+if __name__ == "__main__":
+    main()
